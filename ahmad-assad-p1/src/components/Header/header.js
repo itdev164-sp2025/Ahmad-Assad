3eb18ab0e7f8689f3ebf438b75be3ab663163afc
Ahmad Assad <assada5@gmatc.matc.edu>
2025-04-06 17:15:04 -0500

    Update header.js

--- ahmad-assad-p1/src/components/Header/header.js
+++ ahmad-assad-p1/src/components/Header/header.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Flex, Box } from 'rebass';
 import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import { H1 } from '../Heading';
 
-export const Header = () => {
-  return (
-    <Flex
-      as="header"
-      px={3}
-      py={2}
-      alignItems="center"
-      justifyContent="space-between"
-      bg="primary"
-      color="white"
-    >
-      <Box as={Link} to="/" sx={{ textDecoration: 'none', fontSize: 3 }}>
-        My Gatsby Site
-      </Box>
-      <Box as="img"
-        alt="Gatsby logo"
-        height={20}
-        sx={{ margin: 0 }}
-        src="data:image/svg+xml,%3Csvg fill='none' viewBox='0 0 107 28' xmlns='http://www.w3.org/2000/svg'%3E%3C/g%3E%3C/svg%3E"
-      />
-    </Flex>
-  );
+const Header = ({ TestSite }) => (
+  <header>
+    <H1>
+      <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
+        {TestSite}
+      </Link>
+    </H1>
+  </header>
+);
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 };
+
+Header.defaultProps = {
+  siteTitle: 'TestSite',
+};
+
+export default Header;
